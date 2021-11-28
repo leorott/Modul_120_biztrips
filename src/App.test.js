6fc6099a044786c9e64b576d8223774f9f854d7f
lcavuoti <lcavuoti@clip.ch>
2021-11-28 21:01:40 +0100

    dummy tests added

--- src/App.test.js
+++ src/App.test.js
@@ -3,3 +3,24 @@ import App from './App';
 import useFetch from "./services/useFetch";
 
 
+//
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+//----
+  const sum = function sum(a, b) {
+    return a + b;
+  }
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+//----
+  test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+  });
+
