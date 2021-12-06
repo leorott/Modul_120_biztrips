cfb3a64030605d889e30cc2d0b5672ca707f86e7
lcavuoti <lcavuoti@clip.ch>
2021-12-06 15:55:17 +0100

    one change

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import useFetch from "./services/useFetch";
-
+import {getBusinessTrips} from "./services/tripsService";
 
 //
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
+// test('the fetch fails with an error', async () => {
+//   await expect(getBusinessTrips()).rejects.toMatch('error');
 // });
 
+// test('the data is peanut butter', async () => {
+//   await expect(getBusinessTrips()).resolves.toContain('San Francisco World Trade Center on new Server/IOT/Client ');
+// });
+
+//
+// test('the data is peanut butter', async () => {
+//   await expect(useFetch()).resolves.toBe('peanut butter');
+// });
 
 //----
   const sum = function sum(a, b) {
